f2cf4a0fddada3d7cfde28c057560b0ffd3c81fa
areebarindani-github-account <arsalanabdulqadir784@gmail.com>
2024-03-27 04:57:23 +0500

    Adding a Simple CLI Calculator Project

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node 
+
+import inquirer from "inquirer";
+
+// Printing a wellcome Message
+ console.log("\n\tWellcome To \'CodeWithAreeba\' - CLI Simple Calculator\n");
+
+// Asking question from users through Inquirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber"},
+    {message: "Enter Second Number", type: "number", name: "SecondNumber"},
+    {
+        message: "Select one operator to perform Operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division",],
+    },
+
+]);
+
+// conditional statements If-Else
+
+if(answers.operator === "Addition"){
+console.log(answers.firstNumber + answers.SecondNumber)
+}
+else if (answers.operator === "Subtractions"){
+    console.log(answers.firstNumber - answers.SecondNumber)
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.SecondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.SecondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
